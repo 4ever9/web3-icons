5b6a1692b28af524c6dfbafb44be01457599eaea
Aiden X <caichao.xu@gmail.com>
2024-05-17 23:54:01 +0800

    feat: init project

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+export { ChainEthereumIcon } from "./chains/EthereumIcon";
+export { ChainArbitrumIcon } from "./chains/ArbitrumIcon";
+export { ChainBNBIcon } from "./chains/BNBIcon";
+export { ChainPolygonIcon } from "./chains/PolygonIcon";
+export { ChainAvalancheIcon } from "./chains/AvalancheIcon";
